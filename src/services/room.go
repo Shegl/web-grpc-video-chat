de9e0b34ee515baeaf18718be9cd745c9e966413
Alexander <a.scheglakov@esportsconstruct.com>
2023-07-12 13:39:00 +0200

    clean up

--- src/services/room.go
+++ src/services/room.go
@@ -9,20 +9,21 @@ import (
 
 type RoomService struct {
 	rooms    map[uuid.UUID]*dto.Room
-	asGuests map[uuid.UUID]*dto.Room
+	asAuthor map[uuid.UUID]*dto.Room
+	asGuest  map[uuid.UUID]*dto.Room
 	mu       sync.RWMutex
 }
 
 func (r *RoomService) Create(user *dto.User) (*dto.Room, error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	if room, exists := r.rooms[user.UUID]; exists {
+	if room, exists := r.asAuthor[user.UUID]; exists {
 		// room already exists
 		// let user get back to his room,
 		// if he needs new room, he will get that he needs leave his room first
 		return room, nil
 	}
-	if _, exists := r.asGuests[user.UUID]; exists {
+	if _, exists := r.asGuest[user.UUID]; exists {
 		// let's check user, what if he already as guest in someone else room
 		r.leave(user)
 	}
@@ -37,19 +38,22 @@ func (r *RoomService) create(user *dto.User) *dto.Room {
 		Guest:  nil,
 	}
 	r.rooms[user.UUID] = room
+	r.asAuthor[user.UUID] = room
 	return room
 }
 
 func (r *RoomService) Join(roomUUID uuid.UUID, user *dto.User) (*dto.Room, error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	if room, exists := r.rooms[user.UUID]; exists {
+	if room, exists := r.rooms[roomUUID]; exists {
 		// room exists, good. Is it occupied?
-		if room.Guest == nil {
+		if room.Guest == nil && room.Author != user {
 			return r.join(roomUUID, user), nil
 		} else if room.Guest == user {
 			// same user, its counter as rejoin
 			return room, nil
+		} else if room.Author == user {
+			return nil, errors.New("User cant join as guest to his room. ")
 		} else {
 			return nil, errors.New("Room occupied. ")
 		}
@@ -58,7 +62,7 @@ func (r *RoomService) Join(roomUUID uuid.UUID, user *dto.User) (*dto.Room, error
 }
 
 func (r *RoomService) join(roomUUID uuid.UUID, user *dto.User) *dto.Room {
-	if room, exists := r.rooms[user.UUID]; exists {
+	if room, exists := r.rooms[roomUUID]; exists {
 		room.Guest = user
 		return room
 	}
@@ -68,23 +72,24 @@ func (r *RoomService) join(roomUUID uuid.UUID, user *dto.User) *dto.Room {
 func (r *RoomService) Leave(user *dto.User) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	if room, exists := r.asGuests[user.UUID]; exists {
+	if room, exists := r.asGuest[user.UUID]; exists {
 		room.Guest = nil
-		delete(r.asGuests, user.UUID)
+		delete(r.asGuest, user.UUID)
 	}
 }
 
 func (r *RoomService) leave(user *dto.User) {
-	if room, exists := r.asGuests[user.UUID]; exists {
+	if room, exists := r.asGuest[user.UUID]; exists {
 		room.Guest = nil
-		delete(r.asGuests, user.UUID)
+		delete(r.asGuest, user.UUID)
 	}
 }
 
 func NewRoomService() *RoomService {
 	return &RoomService{
 		rooms:    make(map[uuid.UUID]*dto.Room),
-		asGuests: make(map[uuid.UUID]*dto.Room),
+		asAuthor: make(map[uuid.UUID]*dto.Room),
+		asGuest:  make(map[uuid.UUID]*dto.Room),
 		mu:       sync.RWMutex{},
 	}
 }
